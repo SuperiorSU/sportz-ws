f0d47c256259dbb7b974dbe41c312c233feac4f7
SuperiorSU <sujalsinha2001@gmail.com>
2026-09-03 18:57:03 +0530

    implemented matches POST and GET routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchRouter } from './routes/matches.js';
 const PORT = 8000;
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res)=>{
     res.send("Welcome to local server.");
 })
+
+app.use('/matches', matchRouter)
 
 app.listen(PORT, ()=>{
     console.log("Server is running at PORT", PORT);
